54d6cded68fc48f613b2c7a251b048b307b8ac30
RealSoerensen <patricklykke@live.dk>
2021-11-18 14:42:23 +0100

    Added auto purchase for upgrades and store items. Added autosave feature

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from time import sleep
+from random import randint
+import threading
 
 service = Service("C:\Program Files (x86)\chromedriver.exe")
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
+save_path = "C:\Program Files (x86)"
 
 class CookieBot:
     def __init__(self):
         self.driver = webdriver.Chrome(service=service, options=options)
-
+        try:
+            open(save_path + "\\RobotDucklingBakery.txt")
+        except FileNotFoundError:
+            pass
+        
     def start(self):
         self.driver.get("https://orteil.dashnet.org/cookieclicker/")
         sleep(2)
@@ -20,17 +26,40 @@ class CookieBot:
 
     def cookie_click(self):
         while True:
-            for product_num in range(17, -1, -1):
-                print(product_num)
-                if self.driver.find_element(By.ID, "product" + str(product_num)).get_attribute("class") == "product " \
-                                                                                                           "unlocked " \
-                                                                                                           "enabled":
-                    CookieBot.store(self, "product" + str(product_num))
             self.driver.find_element(By.ID, "bigCookie").click()
 
-    def store(self, item):
-        self.driver.find_element(By.ID, item).click()
+            if randint(0, 100) < 5:
+                CookieBot.upgrade(self)
+                CookieBot.store(self)
 
+    def store(self):
+        for product_num in range(17, -1, -1):
+            try: 
+                store = self.driver.find_element(By.ID, "product" + str(product_num))
+                if store.get_attribute("class") == "product unlocked enabled":
+                    store.click()       
+            except:
+                pass
+    
+    def upgrade(self):
+        try: 
+            upgrade = self.driver.find_element(By.ID, "upgrade0")
+            if upgrade.get_attribute("class") == "crate upgrade enabled":
+                upgrade.click()
+        except:
+            pass
+    
+    def autosave(self):
+        while True:
+            try:
+                self.driver.find_element(By.ID, "prefsButton").click()
+                self.driver.find_element(By.ID, "option").click()
+                save_text = self.driver.find_element(By.ID, "textareaPrompt").text
+                with open(save_path + "\\RobotDucklingBakery.txt", "w") as save:
+                    save.write(save_text)
+                self.driver.find_element(By.CLASS_NAME, "close menuClose").click()
+            except:
+                pass
 
 bot = CookieBot()
 bot.start()
