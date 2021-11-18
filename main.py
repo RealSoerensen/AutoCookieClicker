a204d49af96823e1cc2e167f824ef4e679798817
RealSoerensen <patricklykke@live.dk>
2021-11-19 00:08:41 +0100

    New auto import feature
    New auto import feature when starting. Autosaves now works.

--- main.py
+++ main.py
@@ -1,32 +1,55 @@
+import os
+from random import randint
+from time import sleep
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from time import sleep
-from random import randint
-import threading
 
 service = Service("C:\Program Files (x86)\chromedriver.exe")
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
-save_path = "C:\Program Files (x86)"
+directory = "CookieClickerSave"
+parent_dir = os.path.dirname(__file__)
+path = os.path.join(parent_dir, directory)
+if not os.path.exists(path):
+    os.makedirs(path)
+
 
 class CookieBot:
     def __init__(self):
         self.driver = webdriver.Chrome(service=service, options=options)
-        try:
-            open(save_path + "\\RobotDucklingBakery.txt")
-        except FileNotFoundError:
-            pass
-        
+
     def start(self):
         self.driver.get("https://orteil.dashnet.org/cookieclicker/")
         sleep(2)
+        CookieBot.import_save(self)
+
+    def import_save(self):
+        try:
+            with open(path + "\\RobotDucklingBakery.txt", "r") as save_file:
+                save_text = save_file.read()
+                self.driver.find_element(By.ID, "prefsButton").click()
+                self.driver.find_element(By.XPATH,
+                                         "/html/body/div[2]/div[2]/div[18]/div[2]/div[4]/div[3]/div[3]/a[2]").click()
+                self.driver.find_element(By.XPATH,
+                                         "/html/body/div[2]/div[2]/div[11]/div/div[1]/div[2]/textarea").send_keys(
+                    save_text)
+                self.driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div[11]/div/div[1]/div[3]/a[1]").click()
+                self.driver.find_element(By.XPATH, "/html/body/div/div[2]/div[18]/div[2]/div[4]/div[1]").click()
+                print("Loaded save")
+        except Exception:
+            pass
+
         CookieBot.cookie_click(self)
 
     def cookie_click(self):
         while True:
             self.driver.find_element(By.ID, "bigCookie").click()
+
+            if randint(0, 800) == 400:
+                CookieBot.autosave(self)
 
             if randint(0, 100) < 5:
                 CookieBot.upgrade(self)
@@ -34,32 +57,38 @@ class CookieBot:
 
     def store(self):
         for product_num in range(17, -1, -1):
-            try: 
+            try:
                 store = self.driver.find_element(By.ID, "product" + str(product_num))
                 if store.get_attribute("class") == "product unlocked enabled":
-                    store.click()       
-            except:
+                    store.click()
+            except Exception:
                 pass
-    
+
     def upgrade(self):
-        try: 
+        try:
             upgrade = self.driver.find_element(By.ID, "upgrade0")
             if upgrade.get_attribute("class") == "crate upgrade enabled":
                 upgrade.click()
-        except:
+        except Exception:
             pass
-    
+
     def autosave(self):
-        while True:
-            try:
-                self.driver.find_element(By.ID, "prefsButton").click()
-                self.driver.find_element(By.ID, "option").click()
-                save_text = self.driver.find_element(By.ID, "textareaPrompt").text
-                with open(save_path + "\\RobotDucklingBakery.txt", "w") as save:
-                    save.write(save_text)
-                self.driver.find_element(By.CLASS_NAME, "close menuClose").click()
-            except:
-                pass
+        try:
+            self.driver.find_element(By.ID, "prefsButton").click()
+            self.driver.find_element(By.XPATH,
+                                     "/html/body/div[2]/div[2]/div[18]/div[2]/div[4]/div[3]/div[3]/a[1]").click()
+            save_text = self.driver.find_element(By.XPATH,
+                                                 "/html/body/div/div[2]/div[11]/div/div[1]/div[2]/textarea").text
+            with open(path + "\\RobotDucklingBakery.txt", "w") as save_file:
+                save_file.write(str(save_text))
+            self.driver.find_element(By.ID, "promptOption0").click()
+            self.driver.find_element(By.XPATH, "/html/body/div/div[2]/div[18]/div[2]/div[4]/div[1]").click()
+            print("Saved")
+            sleep(300)
+        except Exception:
+            print("Failed to save")
+            pass
+
 
 bot = CookieBot()
 bot.start()
